159eb63a46498a3bd010e24b469bb881fb4c911a
ekitten <ekitten@email.sc.edu>
2021-12-02 19:36:04 -0500

    new merge changes

--- src/App.vue
+++ src/App.vue
@@ -11,45 +11,8 @@
     <router-link to="/rulesregulations">Rules/Regulations</router-link>
   </div>
   <router-view/>
-
-  <div>
-        <form @submit.prevent="register">
-            <h2>Register</h2>
-            <input
-                type="email"
-                placeholder="Email address..."
-                v-model="email"
-            />
-            <input
-                type="password"
-                placeholder="password..."
-                v-model="password"
-            />
-            <button type="submit">Register</button>
-        </form>
-    </div>
-
-  <div>
-        <form @submit.prevent="login">
-            <h2>Login</h2>
-            <input
-                type="email"
-                placeholder="Email address..."
-                v-model="email"
-            />
-            <input
-                type="password"
-                placeholder="password..."
-                v-model="password"
-            />
-            <button type="submit">Login</button>
-        </form>
-    </div>
-
-    <div>
-      <button @click="logout">Logout</button>
-    </div>
-
+  
+  
 </template>
 
 <style>
@@ -76,62 +39,12 @@
 </style>
 
 <script>
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      email: '',
-      password: ''
+  name: 'app'
+  
     }
-  },
-      methods: {
-         register() {
-            firebase
-                .auth()
-                .createUserWithEmailAndPassword(this.email, this.password)
-                .then(() => {
-                    alert('Successfully registered! Please login.');
-                    this.$router.push('/');
-                })
-                .catch(error => {
-                    alert(error.message);
-                });
-        },
-
-
-        login() {
-            firebase
-                .auth()
-                .signInWithEmailAndPassword(this.email, this.password)
-                .then(() => {
-                    alert('Successfully logged in');
-                    this.$router.push('/dashboard');
-                })
-                .catch(error => {
-                    alert(error.message);
-                });
-        },
     
-
-    logout() {
-            firebase
-                .auth()
-                .signOut()
-                .then(() => {
-                    alert('Successfully logged out');
-                    this.$router.push('/');
-                })
-                .catch(error => {
-                    alert(error.message);
-                    this.$router.push('/');
-                });
-        },
-    }
-        
-      
-  }
-    
-
 
 
 </script>
