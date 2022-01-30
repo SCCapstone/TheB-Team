2cd3036037139b8dd580e505d6d95ab529d9df93
Brandon Torres <brandont123@icloud.com>
2022-01-30 18:33:11 -0500

    Revert "Delete src directory"
    This reverts commit b9c5b8346266952fff51850ec26baebffbaac797.

---
+++ src/views/Routedetails.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="routedetails">
+    <hr>
+    <h1>Route Details</h1>
+    <hr>
+    <h3>Shipment Weight, Start Point, Destination, Truck</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Routedetails',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/views/Rulesregulations.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="rulesregulations">
+    <h3>Rules and Regulations:</h3>
+    <form @submit.prevent="onSubmit">
+      <div class="form-group">
+        <label>State: </label>  
+        <input
+          v-model="form.states"
+          class="form-control"
+          required
+        />
+      </div>
+
+      <div class="form-group">
+        <label>Name: </label>
+        <input
+          v-model="form.name"
+          class="form-control"
+          required
+        />
+      </div>
+
+      <div class="form-group">
+        <label>Type: </label>
+        <input
+          v-model="form.type"
+          class="form-control"
+          required
+        />
+      </div>
+        
+      <button type="submit">
+        Create Variable
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { createVariable } from '@/firebase';
+import { reactive } from 'vue';
+export default {
+  name: 'Rulesregulations',
+  props: {
+    msg: String
+  },
+  setup () {
+    const form = reactive({
+      name: '',
+      states: '',
+      type: ''
+    });
+
+    const onSubmit = async () => {
+      await createVariable({ ...form });
+      form.name = '';
+      form.states = '';
+      form.type = '';
+    }
+
+    return { form, onSubmit };
+  }
+}
+</script>
