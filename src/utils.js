14387af89e9e835238b02fd529712151f78291bd
Brandon Carter <bcarter9893@gmail.com>
2022-01-30 21:32:39 -0500

    unit test for sortVariables

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 'use strict';
 
-export const sortVariables = (variables) => {
+function sortVariables (variables) {
     variables.sort((a, b) => (a.name > b.name) ? 1 : -1);
     return variables
 }
+
+module.exports = sortVariables
---
+++ src/utils.test.js
@@ -0,0 +1,14 @@
+const sortVariables = require('./utils')
+
+test('properly sorts variables alphabetically', () => {
+    //creating object to represent users with name attribute
+    const person1 = {name:"Alex"}
+    const person2 = {name:"Brian"}
+    const person3 = {name:"Carter"}
+    const person4 = {name:"Oliver"}
+    //unsorted array of people's names and the sorted one
+    const names = [person4, person1, person3, person2]
+    const nameSort = [person1, person2, person3, person4]
+
+    expect(sortVariables(names)).toEqual(nameSort)
+})
