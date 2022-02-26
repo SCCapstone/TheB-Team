cdcb02df66eca8080a13731026d939aff2b6960a
Brandon Torres <55719532+bttorres@users.noreply.github.com>
2022-02-25 20:59:47 -0500

    Add files via upload

---
+++ src/views/Rulesregulations.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="rulesregulations">
+    <h3>Rules and Regulations:</h3>
+    <form @submit.prevent="onSubmit">
+      <div class="form-group">
+        <label>State: </label>  
+        <input
+          v-model="form.states"
+          class="form-control"
+          required
+        />
+      </div>
+
+      <div class="form-group">
+        <label>Name: </label>
+        <input
+          v-model="form.name"
+          class="form-control"
+          required
+        />
+      </div>
+
+      <div class="form-group">
+        <label>Type: </label>
+        <input
+          v-model="form.type"
+          class="form-control"
+          required
+        />
+      </div>
+        
+      <button type="submit">
+        Create Variable
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { createVariable, getVariables } from '@/firebase';
+import { sortVariables } from '@/utils';
+import { reactive } from 'vue';
+export default {
+  name: 'Rulesregulations',
+  props: {
+    msg: String
+  },
+  setup () {
+    const form = reactive({
+      name: '',
+      states: '',
+      type: ''
+    });
+
+    const onSubmit = async () => {
+      const items = getVariables();
+      const sorted = sortVariables(items);
+      console.log(sorted);
+      createVariable({ ...form });
+      form.name = '';
+      form.states = '';
+      form.type = '';
+    }
+
+    return { form, onSubmit };
+  }
+}
+</script>
