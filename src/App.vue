e505e86a8151b81fe14346cf7589ed3daf397780
Brandon Torres <brandont123@icloud.com>
2022-03-24 14:00:26 -0400

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div id="nav" align="center">
     <router-link to="/TheB-Team/loginsignup">Log in/Sign up</router-link> |
     <router-link to="/TheB-Team/gps">GPS</router-link> |
@@ -44,4 +44,45 @@ export default {
     
 
 
+</script> -->
+<script>
+import Sidebar from '@/components/Sidebar'
+import { sidebarWidth2 } from '@/components/state'
+export default {
+  components: { Sidebar },
+  setup() {
+    return { sidebarWidth2 }
+  }
+}
 </script>
+<template>
+  <Sidebar />
+  <div :style="{ 'margin-left': sidebarWidth2 }">
+    <router-view />
+    <HereMap  :center="center" />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: left : sidebarWidth;*/
+  color: #000000;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
